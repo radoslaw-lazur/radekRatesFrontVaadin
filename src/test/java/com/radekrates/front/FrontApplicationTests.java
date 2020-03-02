d50575cdc3072cdd1be586dca320f33a7e895761
radoslaw-lazur <radoslaw.lazur@gmail.com>
2020-03-02 20:55:12 +0100

    first commit

---
+++ src/test/java/com/radekrates/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radekrates.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
